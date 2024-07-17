6bfd9d03fc77d5d2dd441bac4b34eeb02e7732a8
Manuel <milton.sanchezrios88@gmail.com>
2024-07-16 22:51:49 -0500

    chatMessage actualizado

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -2,7 +2,7 @@
   <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
-      <ChatBubble :its-mine="true" :message="'hola como estas'" />
+      <ChatBubble :its-mine="true" :message="''" />
       <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
     </div>
   </div>
